9f7d48004f79239fa5e4c9af4a31a1d951707128
unknown <jamelyassin84@gmail.com>
2022-02-08 00:38:20 -0800

    changes

--- super-admin/src/app/modules/admin/coaching/coaching.module.ts
+++ super-admin/src/app/modules/admin/coaching/coaching.module.ts
@@ -1,15 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoachingComponent } from './coaching/coaching.component';
 
-
-
 @NgModule({
-  declarations: [
-    CoachingComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [CoachingComponent],
+    imports: [CommonModule],
 })
-export class CoachingModule { }
+export class CoachingModule {}
