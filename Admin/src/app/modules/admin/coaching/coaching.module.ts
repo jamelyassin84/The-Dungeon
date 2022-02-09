fb6df2b467d950caaf9e74812df45aa69ca30670
unknown <jamelyassin84@gmail.com>
2022-02-09 02:58:56 -0800

    our story na lng

--- Admin/src/app/modules/admin/coaching/coaching.module.ts
+++ Admin/src/app/modules/admin/coaching/coaching.module.ts
@@ -4,6 +4,15 @@ import { CoachingComponent } from './coaching/coaching.component'
 import { CoachingSection1Component } from './pages/coaching-section1/coaching-section1.component'
 import { CoachingSection2Component } from './pages/coaching-section2/coaching-section2.component'
 import { RouterModule } from '@angular/router'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { FuseCardModule } from '@fuse/components/card'
+import { FuseAlertModule } from '@fuse/components/alert'
+import { SharedModule } from 'app/shared/shared.module'
 
 @NgModule({
 	declarations: [
@@ -11,6 +20,18 @@ import { RouterModule } from '@angular/router'
 		CoachingSection1Component,
 		CoachingSection2Component,
 	],
-	imports: [CommonModule, RouterModule],
+	imports: [
+		CommonModule,
+		RouterModule,
+		MatButtonModule,
+		MatCheckboxModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatInputModule,
+		MatProgressSpinnerModule,
+		FuseCardModule,
+		FuseAlertModule,
+		SharedModule,
+	],
 })
 export class CoachingModule {}
