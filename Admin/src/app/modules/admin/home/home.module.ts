dcf09fb97629bbdd1f612caf00f1a878a4305772
unknown <jamelyassin84@gmail.com>
2022-02-08 21:41:21 -0800

    home section 1

--- Admin/src/app/modules/admin/home/home.module.ts
+++ Admin/src/app/modules/admin/home/home.module.ts
@@ -1,3 +1,6 @@
+import { FuseAlertModule } from './../../../../@fuse/components/alert/alert.module'
+import { FuseCardModule } from './../../../../@fuse/components/card/card.module'
+import { MatCheckboxModule } from '@angular/material/checkbox'
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HomeComponent } from './home/home.component'
@@ -9,6 +12,10 @@ import { HomeSection3Component } from './pages/home-section3/home-section3.compo
 import { HomeSection4Component } from './pages/home-section4/home-section4.component'
 import { HomeSection5Component } from './pages/home-section5/home-section5.component'
 import { RouterModule } from '@angular/router'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { SharedModule } from 'app/shared/shared.module'
 
 @NgModule({
 	declarations: [
@@ -19,6 +26,18 @@ import { RouterModule } from '@angular/router'
 		HomeSection4Component,
 		HomeSection5Component,
 	],
-	imports: [CommonModule, MatIconModule, MatButtonModule, RouterModule],
+	imports: [
+		CommonModule,
+		MatButtonModule,
+		MatCheckboxModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatInputModule,
+		MatProgressSpinnerModule,
+		FuseCardModule,
+		FuseAlertModule,
+		SharedModule,
+		RouterModule,
+	],
 })
 export class HomeModule {}
--- Admin/src/app/modules/admin/home/pages/home-section1/home-section1.component.ts
+++ Admin/src/app/modules/admin/home/pages/home-section1/home-section1.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { trigger } from '@angular/animations'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-home-section1',
-  templateUrl: './home-section1.component.html',
-  styleUrls: ['./home-section1.component.scss']
+	selector: 'app-home-section1',
+	templateUrl: './home-section1.component.html',
+	styleUrls: ['./home-section1.component.scss'],
 })
 export class HomeSection1Component implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	trigger(id: string) {
+		document.getElementById(id).click()
+	}
 }
