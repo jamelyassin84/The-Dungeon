339eb395755125c568be034395caffed5342fde8
unknown <jamelyassin84@gmail.com>
2022-02-08 23:26:46 -0800

    home

--- Admin/src/app/modules/admin/home/home.tabs.ts
+++ Admin/src/app/modules/admin/home/home.tabs.ts
@@ -1,33 +1,33 @@
 export const HomeTabs: Tab[] = [
-    {
-        id: 1,
-        name: 'HOME BANNER',
-        route: '/home/1',
-    },
-    {
-        id: 2,
-        name: 'SECTION II',
-        route: '/home/2',
-    },
-    {
-        id: 3,
-        name: 'SLIDING IMAGES',
-        route: '/home/3',
-    },
-    {
-        id: 4,
-        name: 'PRICING SECTION',
-        route: '/home/4',
-    },
-    {
-        id: 5,
-        name: 'FOOTER BANNER',
-        route: '/home/5',
-    },
-];
+	{
+		id: 1,
+		name: 'HOME BANNER',
+		route: '/home/1',
+	},
+	{
+		id: 2,
+		name: 'SECTION II',
+		route: '/home/2',
+	},
+	{
+		id: 3,
+		name: 'SLIDING IMAGES',
+		route: '/home/3',
+	},
+	{
+		id: 4,
+		name: 'PRICING SECTION',
+		route: '/home/4',
+	},
+	{
+		id: 5,
+		name: 'FOOTER ',
+		route: '/home/5',
+	},
+]
 
 export interface Tab {
-    id: number;
-    name: string;
-    route: string;
+	id: number
+	name: string
+	route: string
 }
