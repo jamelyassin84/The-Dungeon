5d4448e1b7217874d7ed7c473f54372706d87235
Jamel Yassin <jamelyassin84@gmail.com>
2022-02-12 21:39:09 +0400

    changes

--- Admin/src/app/modules/admin/inbox/inbox.module.ts
+++ Admin/src/app/modules/admin/inbox/inbox.module.ts
@@ -1,15 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { InboxComponent } from './inbox/inbox.component';
-
-
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { InboxComponent } from './inbox/inbox.component'
+import { MatIconModule } from '@angular/material/icon'
+import { RouterModule } from '@angular/router'
+import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { FuseCardModule } from '@fuse/components/card'
+import { FuseAlertModule } from '@fuse/components/alert'
+import { SharedModule } from 'app/shared/shared.module'
 
 @NgModule({
-  declarations: [
-    InboxComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+	declarations: [InboxComponent],
+	imports: [
+		CommonModule,
+		RouterModule,
+		MatButtonModule,
+		MatCheckboxModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatInputModule,
+		MatProgressSpinnerModule,
+		FuseCardModule,
+		FuseAlertModule,
+		SharedModule,
+	],
 })
-export class InboxModule { }
+export class InboxModule {}
